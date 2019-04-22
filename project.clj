7b6bf81930badb4f7b687550f261c2c3d84544bc
Matthew Downey <matthewdowney20@gmail.com>
2019-04-21 21:34:59 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject org.clojars.mjdowney/schemata-s3 "1.0.0"
+  :description "Clojure interface for Amazon S3 via a schemata context."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/matthewdowney/schemata-s3"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojars.mjdowney/schemata "1.0.0"]
+                 [amazonica "0.3.141"]])
